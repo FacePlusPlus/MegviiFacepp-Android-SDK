8521d6e735dc018f42bb080f8e5f0183f8b5453b
Li Yanshun <liyanshun@megvii.com>
2017-05-24 18:07:06 +0800

    添加online auth

--- MGFaceppSDK/MGFaceppSDK/src/main/jni/include/MG_Facepp.h
+++ MGFaceppSDK/MGFaceppSDK/src/main/jni/include/MG_Facepp.h
@@ -46,7 +46,7 @@ typedef enum {
                                             ///< 此模式下人脸检测与跟踪会更平均的使用 CPU 计算资源。
     MG_FPP_DETECTIONMODE_TRACKING_FAST,     ///< 牺牲了人脸关键点的贴合度，提升了人脸跟踪的速度
     MG_FPP_DETECTIONMODE_TRACKING_ROBUST    ///< 提高了人脸关键点的贴合度，降低了人脸跟踪的速度
-} MG_FPP_DETECTIONMODE;
+} MG_FPP_DETECTIONMODE;						///< 检测人脸时只跟踪单张人脸
 
 struct _MG_FPP_API;
 /**
@@ -364,7 +364,7 @@ typedef struct {
      */
     MG_RETCODE (*GetFeatureData)(
         MG_FPP_APIHANDLE api_handle,
-        MG_SINGLE _OUT *feature_data,
+        void _OUT *feature_data,
         MG_INT32 feature_length);
 
     /**
@@ -388,8 +388,8 @@ typedef struct {
      */
     MG_RETCODE (*FaceCompare)(
         MG_FPP_APIHANDLE api_handle,
-        const MG_SINGLE* feature_data1,
-        const MG_SINGLE* feature_data2,
+        const void* feature_data1,
+        const void* feature_data2,
         MG_INT32 feature_length,
         MG_DOUBLE _OUT *score_ptr);
 

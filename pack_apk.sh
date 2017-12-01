7eb6fdb10b50e0cecc9390fe064d83569abfb31c
Xie Jiantao <xiejiantao@megvii.com>
2017-12-01 15:22:52 +0800

    add:打包脚本

---
+++ pack_apk.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#delete tag
+if [ -f "release/tag" ]; then
+    rm release/tag
+fi
+
+#delete apk
+list=$(find ./release -name "MGFaceppDemo*")
+echo $list
+if [ -n "$list" ]; then
+	rm ./release/MGFaceppDemo*
+fi
+
+# build the project
+./gradlew clean
+./gradlew buildAAR
+./gradlew build
+
+#git date
+git rev-parse HEAD > release/tag
+_tag=`git rev-parse --short HEAD`
+_date=`date '+%Y%m%d'`
+
+
+#copy apk
+cp faceppdemo/build/outputs/apk/faceppdemo-debug.apk release/"MGFaceppDemo_0.5.2_${_date}_${_tag}.apk"
+
+#delete build 本地运行注释掉
+rm -rf build
+rm -rf faceppdemo/build
+rm -rf sdk/build
+
+
+
